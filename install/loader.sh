e3a0ec3c1cdba21c8ecb4f79535afcf64cbb0903
Unknown INS <144177491+EvilUnknown@users.noreply.github.com>
2024-02-05 16:48:42 +0700

    update

--- install/loader.sh
+++ install/loader.sh
@@ -1,5 +1,5 @@
 source $APP_INSTALL/bin/Color.sh
-source $APP_INSTALL/bin/Constain.sh
+source $APP_INSTALL/Constain.sh
 source $APP_INSTALL/bin/Core.sh
 source $APP_INSTALL/bin/IP.sh
 source $APP_INSTALL/bin/FireWall.sh
